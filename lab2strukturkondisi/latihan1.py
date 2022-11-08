78ee8d6b8302babe0aca5d3941859413e5f0208a
cahyo <vonixgame22@gmail.com>
2022-11-08 11:55:25 +0700

    Menambahkan file

---
+++ lab2strukturkondisi/latihan1.py
@@ -0,0 +1,8 @@
+# menentukan bilangan
+print('Menentukan bilangan terbesar dari 2 bilangan')
+a = int(input("Masukkan nilai A:"))
+b = int(input("Masukkan nilai B:"))
+if a > b:
+    print("A =", a, "yang terbesar")
+elif b > a:
+    print("B=", b, "yang terbesar")
